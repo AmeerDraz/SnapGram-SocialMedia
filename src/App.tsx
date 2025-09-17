73b3bbb382d4d477fbaba5ff11fa82e6cc6543d6
AmeerDraz <ameerabudraz1@gmail.com>
2025-09-17 12:24:53 +0300

    fix: fix tailwindcss

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,10 @@
 import React from 'react'
-import "./globals.css";
+import './globals.css'
+
 
 const App = () => {
   return (
-    <div className='text-3xl font-bold underline'>Hello Snapgram</div>
+    <div className='text-red-500 text-3xl font-bold underline'>hello SnapGram!</div>
   )
 }
 
--- src/main.tsx
+++ src/main.tsx
@@ -1,7 +1,6 @@
-import ReactDOM from "react-dom/client";
-import App from "./App";
+import ReactDOM from 'react-dom/client'
+import App from './App'
 
-
-ReactDOM.createRoot(document.getElementById("root")!).render(
+ReactDOM.createRoot(document.getElementById('root')!).render(
     <App />
-);
+)
